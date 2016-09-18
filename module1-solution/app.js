82fed527124504735bf075330609cc51f760cec7
Jan Marcis <jan.marcis@4finance.com>
2016-09-18 07:49:53 +0200

    inject mistake

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,26 +1,26 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-MsgController.$inject = ['$scope'];
+    LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController($scope) {
-  $scope.foodList = "5 eggs,3 toasts";
+    function LunchCheckController($scope) {
+        $scope.foodList = "5 eggs,3 toasts";
 
-  $scope.checkFood = function () {
-      if (!$scope.foodList.length) {
-          $scope.checkResult = 'Please enter data first';
-          return;
-      }
-      var exploded = $scope.foodList.split(",");
-      if (exploded.length <=3) {
-          $scope.checkResult = 'Enjoy!';
-      } else if (exploded.length > 3) {
-        $scope.checkResult = 'Too much!';
-      }
-  };
-}
+        $scope.checkFood = function () {
+            if (!$scope.foodList.length) {
+                $scope.checkResult = 'Please enter data first';
+                return;
+            }
+            var exploded = $scope.foodList.split(",");
+            if (exploded.length <= 3) {
+                $scope.checkResult = 'Enjoy!';
+            } else if (exploded.length > 3) {
+                $scope.checkResult = 'Too much!';
+            }
+        };
+    }
 
 })();
